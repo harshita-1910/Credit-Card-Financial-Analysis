e65896ee2ecc10b04e85684fbbd6237025ea22f9
harshita-1910 <harshitaag2003@gmail.com>
2024-11-09 18:46:17 +0530

    Add files via upload

---
+++ Query1.sql
@@ -0,0 +1,53 @@
+CREATE DATABASE creditcard;
+
+CREATE TABLE cc_detail (
+    Client_Num INT,
+    Card_Category VARCHAR(20),
+    Annual_Fees INT,
+    Activation_30_Days INT,
+    Customer_Acq_Cost INT,
+    Week_Start_Date DATE,
+    Week_Num VARCHAR(20),
+    Qtr VARCHAR(10),
+    Current_Year INT,
+    Credit_Limit DECIMAL(10,2),
+    Total_Revolving_Bal INT,
+    Total_Trans_Amt INT,
+    Total_Trans_Ct INT,
+    Avg_Utilization_Ratio DECIMAL(10,3),
+    Use_Chip VARCHAR(10),
+    Exp_Type VARCHAR(50),
+    Interest_Earned DECIMAL(10,3),
+    Delinquent_Acc VARCHAR(5)
+);
+
+CREATE TABLE cust_detail (
+    Client_Num INT,
+    Customer_Age INT,
+    Gender VARCHAR(5),
+    Dependent_Count INT,
+    Education_Level VARCHAR(50),
+    Marital_Status VARCHAR(20),
+    State_Cd VARCHAR(50),
+    Zipcode VARCHAR(20),
+    Car_Owner VARCHAR(5),
+    House_Owner VARCHAR(5),
+    Personal_Loan VARCHAR(5),
+    Contact VARCHAR(50),
+    Customer_Job VARCHAR(50),
+    Income INT,
+    Cust_Satisfaction_Score INT
+);
+
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\credit_card.csv'
+INTO TABLE cc_detail
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\customer.csv'
+INTO TABLE cust_detail
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
